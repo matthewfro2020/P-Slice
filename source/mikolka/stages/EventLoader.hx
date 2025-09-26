55c85084ca9e0101b81b2984a73aa4db7e6d2c90
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-09-26 10:56:14 -0400

    Update EventLoader.hx

--- source/mikolka/stages/EventLoader.hx
+++ source/mikolka/stages/EventLoader.hx
@@ -15,73 +15,83 @@ import mikolka.vslice.components.crash.UserErrorSubstate;
 #end
 #end
 
+import backend.BaseStage;
+import states.PlayState;
+
 class EventLoader extends BaseStage {
     public static var currentStage:BaseStage = null;
+
     #if LUA_ALLOWED
-    public static function implement(funk:FunkinLua)
-        {
-            var lua:State = funk.lua;
-            funk.set('versionPS', MainMenuState.pSliceVersion.trim());
-            Lua_helper.add_callback(lua, "markAsPicoCapable", function(force:Bool = false) {
-                new PicoCapableStage(force);
-            });
-            Lua_helper.add_callback(lua, "changeTransStickers", function(stickerSet:String = null,stickerPack:String = null) {
-                if(stickerSet != null && stickerSet != "") StickerSubState.STICKER_SET = stickerSet;
-                if(stickerPack != null && stickerPack != "") StickerSubState.STICKER_PACK = stickerPack;
-            });
-            Lua_helper.add_callback(lua, "getFreeplayCharacter", function() {
-                return VsliceOptions.LAST_MOD.char_name;
-            });
-            Lua_helper.add_callback(lua, "setFreeplayCharacter", function(character:String,modded:Bool = false) {
-                VsliceOptions.LAST_MOD = {
-                    mod_dir: modded? ModsHelper.getActiveMod() : "",
-                    char_name: character
-                }; //? save selected character
-            });
-        }
+    public static function implement(funk:FunkinLua) {
+        var lua:State = funk.lua;
+        funk.set('versionPS', MainMenuState.pSliceVersion.trim());
+
+        Lua_helper.add_callback(lua, "markAsPicoCapable", function(force:Bool = false) {
+            new PicoCapableStage(force);
+        });
+
+        Lua_helper.add_callback(lua, "changeTransStickers", function(stickerSet:String = null, stickerPack:String = null) {
+            if (stickerSet != null && stickerSet != "") StickerSubState.STICKER_SET = stickerSet;
+            if (stickerPack != null && stickerPack != "") StickerSubState.STICKER_PACK = stickerPack;
+        });
+
+        Lua_helper.add_callback(lua, "getFreeplayCharacter", function() {
+            return VsliceOptions.LAST_MOD.char_name;
+        });
+
+        Lua_helper.add_callback(lua, "setFreeplayCharacter", function(character:String, modded:Bool = false) {
+            VsliceOptions.LAST_MOD = {
+                mod_dir: modded ? ModsHelper.getActiveMod() : "",
+                char_name: character
+            };
+        });
+    }
     #end
+
     public static function addstage(name:String) {
         var addNene = true;
         currentStage = null;
 
-        if(VsliceOptions.LEGACY_BAR) new LegacyScoreBars();
+        if (VsliceOptions.LEGACY_BAR) new LegacyScoreBars();
         new VSliceEvents();
-        if(name == "tank" || name == "tankmanBattlefieldErect") new TankmanStagesAddons();
+        if (name == "tank" || name == "tankmanBattlefieldErect") new TankmanStagesAddons();
 
-        currentStage = switch (name)
-		{
-			case 'stage': new StageWeek1(); 						//Week 1
-			case 'spooky': new Spooky();							//Week 2
-			case 'philly': new Philly();							//Week 3
-			case 'limo': new Limo();								//Week 4
-			case 'mall': new Mall();								//Week 5 - Cocoa, Eggnog
-			case 'mallEvil': new MallEvil();						//Week 5 - Winter Horrorland
-			case 'school': new School();							//Week 6 - Senpai, Roses
-			case 'schoolEvil': new SchoolEvil();					//Week 6 - Thorns
-			case 'tank': new Tank();								//Week 7 - Ugh, Guns, Stress
-			case 'phillyStreets': new PhillyStreets(); 				//Weekend 1 - Darnell, Lit Up, 2Hot
-			case 'phillyBlazin': new PhillyBlazin();				//Weekend 1 - Blazin
-			case 'mainStageErect': new MainStageErect();			//Week 1 Special 
-			case 'spookyMansionErect': new SpookyMansionErect();	//Week 2 Special 
-			case 'phillyTrainErect': new PhillyTrainErect();  		//Week 3 Special 
-			case 'limoRideErect': new LimoRideErect();  			//Week 4 Special 
-			case 'mallXmasErect': new MallXmasErect(); 				//Week 5 Special 
-			case 'schoolErect': new SchoolErect();					//Week 6 Special - Erect Mode
-			case 'schoolPico': new SchoolErect();					//Week 6 Special - Pico
-			case 'schoolEvilErect': new SchoolEvilErect();			//Week 6 Special - Thorns
-			case 'tankmanBattlefieldErect': new TankErect();		//Week 7 Special
-			case 'phillyStreetsErect': new PhillyStreetsErect(); 	//Weekend 1 Special
-			case 'drippypopErect': new DrippyPopErectStage(); 	//drippy pop Special 
+        currentStage = switch (name) {
+            case 'stage': new StageWeek1();
+            case 'spooky': new Spooky();
+            case 'philly': new Philly();
+            case 'limo': new Limo();
+            case 'mall': new Mall();
+            case 'mallEvil': new MallEvil();
+            case 'school': new School();
+            case 'schoolEvil': new SchoolEvil();
+            case 'tank': new Tank();
+            case 'phillyStreets': new PhillyStreets();
+            case 'phillyBlazin': new PhillyBlazin();
+
+            // Erect Specials
+            case 'mainStageErect': new MainStageErect();
+            case 'spookyMansionErect': new SpookyMansionErect();
+            case 'phillyTrainErect': new PhillyTrainErect();
+            case 'limoRideErect': new LimoRideErect();
+            case 'mallXmasErect': new MallXmasErect();
+            case 'schoolErect': new SchoolErect();
+            case 'schoolPico': new SchoolErect(); // reuse
+            case 'schoolEvilErect': new SchoolEvilErect();
+            case 'tankmanBattlefieldErect': new TankErect();
+            case 'phillyStreetsErect': new PhillyStreetsErect();
+            case 'drippypopErect': new DrippyPopErectStage();
+
             default: null;
-		};
-        if(currentStage == null) addNene = false;
+        };
 
-        if(addNene && PicoCapableStage.instance == null) {
+        if (currentStage == null) addNene = false;
+
+        if (addNene && PicoCapableStage.instance == null) {
             var pico = new PicoCapableStage();
             var game = PlayState.instance;
             game.stages.remove(pico);
-            game.stages.insert(game.stages.length-2,pico);
+            game.stages.insert(game.stages.length - 2, pico);
         }
-        
-    } 
+    }
 }
