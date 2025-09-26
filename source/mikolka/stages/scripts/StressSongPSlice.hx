3c724eb64863ddafbda427aba4df2655deaf9179
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-26 13:46:02 -0400

    Update StressSongPSlice.hx

--- source/mikolka/stages/scripts/StressSongPSlice.hx
+++ source/mikolka/stages/scripts/StressSongPSlice.hx
@@ -28,7 +28,7 @@ class StressSongPSlice {
 
     // Called from TankErect.new()
     public function onCreate():Void {
-        var song = PS.SONG.song.toLowerCase();
+        var song = PlayState.SONG.song.toLowerCase();
         if (song.contains("stress")) {
             trace("StressSongPSlice: onCreate called for " + song);
         }
@@ -36,7 +36,7 @@ class StressSongPSlice {
 
     // Start countdown setup
     public function onCountdownStart():Void {
-        var song = PS.SONG.song.toLowerCase();
+        var song = PlayState.SONG.song.toLowerCase();
 
         if (song.contains("gooey") && !hasPlayedCutscene) {
             hasPlayedCutscene = true;
@@ -91,7 +91,6 @@ class StressSongPSlice {
         var shader = new AdjustColorShader();
         shader.brightness = -10;
         shader.saturation = -20;
-        bgSprite.shader = shader;
 
         // Fake timer that will end cutscene after ~5s
         cutsceneTimer = new FlxTimer().start(5, function(tmr:FlxTimer) {
@@ -103,10 +102,7 @@ class StressSongPSlice {
         trace("StressSongPSlice: ending gooey cutscene");
         skipCutscene();
 
-        // Start the song countdown if still in cutscene phase
-        if (PS.inCutscene) PS.inCutscene = false;
-        if (!PS.startedCountdown) {
-            PS.startCountdown();
-        }
+        // Start the song countdown if not already started
+        PlayState.instance.startCountdown();
     }
 }
